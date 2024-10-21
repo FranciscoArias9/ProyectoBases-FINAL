eab730ecce76c4f9157092aa639c90d562f5ea7c
Joseph <74473791+sephf@users.noreply.github.com>
2024-10-21 13:59:23 -0600

    Create roles_usuario.sql

---
+++ Proyecto-AdminBases-main/Admin-bases-main/backend/roles_usuario.sql
@@ -0,0 +1,42 @@
+-- Crear roles
+CREATE ROLE c##rol_instructor;
+CREATE ROLE c##rol_cliente;
+CREATE ROLE c##rol_soporte;
+
+-- Crear usuarios
+CREATE USER c##instructor1 IDENTIFIED BY contrasena_instructor1;
+CREATE USER c##instructor2 IDENTIFIED BY contrasena_instructor2;
+
+CREATE USER c##cliente1 IDENTIFIED BY contrasena_cliente1;
+CREATE USER c##cliente2 IDENTIFIED BY contrasena_cliente2;
+
+CREATE USER c##soporte1 IDENTIFIED BY contrasena_soporte1;
+
+-- Asignar roles a usuarios
+GRANT c##rol_instructor TO c##instructor1;
+GRANT c##rol_instructor TO c##instructor2;
+
+GRANT c##rol_cliente TO c##cliente1;
+GRANT c##rol_cliente TO c##cliente2;
+
+GRANT c##rol_soporte TO c##soporte1;
+
+-- Asignar privilegios a roles
+GRANT SELECT, INSERT, UPDATE ON cursos TO c##rol_instructor;
+GRANT SELECT, INSERT, UPDATE ON rutinas TO c##rol_instructor;
+GRANT SELECT, INSERT, UPDATE ON maquinas TO c##rol_instructor;
+GRANT SELECT ON clientes TO c##rol_instructor; -- Cambié "cliente" a "clientes"
+GRANT INSERT ON historial_curso TO c##rol_instructor;
+GRANT SELECT ON bitacora TO c##rol_instructor;
+
+GRANT SELECT ON cursos TO c##rol_cliente;
+GRANT INSERT, UPDATE ON clientes TO c##rol_cliente; -- Cambié "cliente" a "clientes"
+GRANT INSERT ON historial_curso TO c##rol_cliente;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON cursos TO c##rol_soporte;
+GRANT SELECT, INSERT, UPDATE, DELETE ON rutinas TO c##rol_soporte;
+GRANT SELECT, INSERT, UPDATE, DELETE ON maquinas TO c##rol_soporte;
+GRANT SELECT, INSERT, UPDATE, DELETE ON clientes TO c##rol_soporte; -- Cambié "cliente" a "clientes"
+GRANT SELECT, INSERT, UPDATE, DELETE ON instructores TO c##rol_soporte;
+GRANT SELECT, INSERT, UPDATE, DELETE ON historial_curso TO c##rol_soporte;
+GRANT SELECT, INSERT, UPDATE, DELETE ON bitacora TO c##rol_soporte;
