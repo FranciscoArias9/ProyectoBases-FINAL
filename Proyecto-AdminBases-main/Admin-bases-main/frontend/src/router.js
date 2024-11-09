7b7d14cf384ca37dc96aca74298fa88f6ada625d
Francisco Arias Sanabria <104183291+FranciscoArias9@users.noreply.github.com>
2024-11-08 23:48:24 -0600

    Update router.js

--- Proyecto-AdminBases-main/Admin-bases-main/frontend/src/router.js
+++ Proyecto-AdminBases-main/Admin-bases-main/frontend/src/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import AppIngreso from './components/AppIngreso.vue';
 import AppContactos from './components/AppContactos.vue';
-import ServicioInscripcion from './components/servicios/ServicioInscripcion.vue';
+//import ServicioInscripcion from './components/servicios/ServicioInscripcion.vue';
 import AppInicio from './components/AppInicio.vue';
 import ServicioCurso from './components/servicios/ServicioCurso.vue';
 import AgregarCurso from './components/servicios/AgregarCurso.vue';
@@ -13,11 +13,13 @@ import MantenimientoClientes from './components/MantenimientoClientes.vue';
 import MantenimientoMaquinas from './components/MantenimientoMaquinas.vue';
 import MantenimientoCursos from './components/MantenimientoCursos.vue';
 import MantenimientoRutinas from './components/MantenimientoRutinas.vue';
+
+import InscripcionForm from './components/InscripcionForm.vue';
 const routes = [
   { path: '/', component: AppInicio },
   { path: '/contactos', component: AppContactos },
   { path: '/ingreso', component: AppIngreso },
-  { path: '/servicios/inscripcion', component: ServicioInscripcion },
+  //{ path: '/servicios/inscripcion', component: ServicioInscripcion },
   { path: '/servicios/cursos', component: ServicioCurso },
   { path: '/servicios/agregar-curso', component: AgregarCurso }, // Nueva ruta para agregar curso
   { path: '/servicios/rutinas', component: MantenimientoRutina }, // Nueva ruta para mantenimiento de rutinas
@@ -28,7 +30,10 @@ const routes = [
   { path: '/clientes/mantenimiento', component: MantenimientoClientes },  
   { path: '/maquinas/mantenimiento', component: MantenimientoMaquinas },
   { path: '/cursos/mantenimiento', component: MantenimientoCursos },
-  { path: '/rutinas/mantenimiento', component: MantenimientoRutinas }
+  { path: '/rutinas/mantenimiento', component: MantenimientoRutinas },
+  // RUTA DE CLIENTE - INSCRIPCION
+  { path: '/servicios/inscripcion', component: InscripcionForm}
+
 
 ];
 
